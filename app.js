dd6c3c0175e53937e3ca9ad3b59ec5ce0fe2c11c
MatthewSimmons <matthewarthursimmons@gmail.com>
2025-06-09 15:13:49 -0400

    Can view books hard coded

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// Query Selectors
+const bookCardContainer = document.querySelector('.book-card-container');
+// Library Array for storing books
+const myLibrary = [];
+// Programmatically create elements
+const newDiv = document.createElement("div");
+
+
 function Book(title, author, pages, read) {
     if (!new.target){
         throw Error("You must use the 'new' operator to call this contructor");
@@ -13,3 +21,32 @@ function Book(title, author, pages, read) {
     };
 }
 
+function addBookToLibrary(title, author, pages, read){
+    newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook);
+}
+
+bookTest = new Book("asdfasfd", "asdfqewr", "195", "read");
+
+myLibrary.push(bookTest);
+myLibrary.push(bookTest);
+myLibrary.push(bookTest);
+myLibrary.push(bookTest);
+myLibrary.push(bookTest);
+myLibrary.push(bookTest);
+myLibrary.push(bookTest);
+myLibrary.push(bookTest);
+myLibrary.push(bookTest);
+myLibrary.push(bookTest);
+myLibrary.push(bookTest);
+
+function displayBooks(){
+    myLibrary.forEach(item => {
+        const div = document.createElement("div");
+        div.classList.add("book-card");
+        bookCardContainer.appendChild(div);
+        div.textContent = item.info()
+    })
+}
+
+displayBooks();
